f871ff1de9f68c8157e99b1ef71ff2069dbfa217
The-sum <91456161+The-sum@users.noreply.github.com>
2023-01-03 15:21:14 +0800

    commit pull

--- src/main/java/the/sum/Main.java
+++ src/main/java/the/sum/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("github");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
